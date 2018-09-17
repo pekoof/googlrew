f7ce9a236581e1146ab87793e7036d4a6ca8fa94
pekoof <43298720+pekoof@users.noreply.github.com>
2018-09-17 17:02:43 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,67 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '+'
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Alpha Codes `,"http://twitch.tv/Alpha Codes")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+
+
+
+
+
+
+
+client.on('message', message => {
+if (message.content.startsWith('كود البنكر')){
+     let ra3d = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("random")
+  .setDescription(" **00085**")
+     
+     
+  message.channel.sendEmbed(ra3d);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
