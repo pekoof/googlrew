28f191458bdd8e69239717752cbaeccca596d56d
pekoof <43298720+pekoof@users.noreply.github.com>
2020-03-07 00:15:07 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,7 +40,7 @@ if (message.content.startsWith('code bunker')){
      let ra3d = new Discord.RichEmbed()
   .setAuthor(message.author.username)
   .setColor("RANADOM")
-  .setDescription(" **04280**")
+  .setDescription(" **70755**")
      
      
   message.channel.sendEmbed(ra3d);
